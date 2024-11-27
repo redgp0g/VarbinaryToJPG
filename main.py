49d44559fea76a9f679c6b2774a84eed1aff54fe
Guilherme Gordiano <guilhermeg2004@gmail.com>
2024-11-27 09:40:54 -0300

    :recycle: refactor Melhorar código - Criar um txt com os funcionários sem foto e o id deles

--- main.py
+++ main.py
@@ -8,21 +8,28 @@ conn_str = os.getenv("STRING_CONNECTION")
 
 conn = pyodbc.connect(conn_str)
 
-query = f"SELECT nome,foto FROM funcionario WHERE ativo = 1"
+query = """
+SELECT DISTINCT F.IDFuncionario, F.nome, F.foto 
+FROM Funcionario AS F 
+"""
 
 cursor = conn.cursor()
 cursor.execute(query)
+
 funcionarios = cursor.fetchall()
 
-for funcionario in funcionarios:
-    if funcionario.foto is not None:
-        image_data = funcionario.foto
-        
-        caminho_imagem = os.path.join(os.getcwd(),"Fotos\\" + funcionario.nome + ".jpg")
-        
-        with open(caminho_imagem, 'wb') as foto:
-            foto.write(image_data)
-        print("Imagem salva com sucesso em:", caminho_imagem)
-    else:
-        print("Nenhuma imagem encontrada para a pessoa", funcionario.nome)
+# Abre (ou cria) o arquivo txt para registrar nomes e IDs de quem não tem foto
+with open("funcionarios_sem_foto.txt", 'w', encoding='utf-8') as txt_file:
+    for funcionario in funcionarios:
+        if funcionario.foto is not None:
+            image_data = funcionario.foto
+            
+            image_path = os.path.join(os.getcwd(), "Fotos", f"{funcionario.nome}.jpg")
+
+            with open(image_path, 'wb') as foto_file:
+                foto_file.write(image_data)
+            print("Imagem salva com sucesso em:", image_path)
+        else:
+            txt_file.write(f"ID: {funcionario.IDFuncionario}, Nome: {funcionario.nome}\n")
+            print(f"Nenhuma imagem encontrada para a pessoa {funcionario.nome}")
 conn.close()
